9da4e869c120b2294712865eb52a2937be4b0d99
nixena <nikanozadze1010@gmail.com>
2023-08-26 16:37:34 +0400

    feat: create category & listing models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+class Listing(models.Model):
+    title = models.CharField(max_length=35)
+    description = models.CharField(max_length=500)
+    imageUrl = models.CharField(max_length=1500)
+    price = models.FloatField()
+    active = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
